5c8a85592f3aae6d9cda9a16f402efdfb9e6aaa1
PratikAwaik <pratikawaik125@gmail.com>
2024-10-19 04:04:10 +0530

    start on mobile responsive headr

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,21 +3,32 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="w-screen flex items-center justify-between p-4">
-      <Link href={"/"} className="text-2xl font-bold">
-        Brainwave.io
-      </Link>
-      <nav>
-        <ul className="flex items-center gap-x-10">
-          {navLinks.map((link) => (
-            <li key={link.title} className="font-bold text-sm hover:underline">
-              <Link href={link.href}>{link.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <Button className="font-bold">Buy now - Starting at $99</Button>
-    </header>
+    <>
+      <header className="w-screen items-center justify-between p-4 hidden md:flex">
+        <Link href={"/"} className="text-2xl font-bold">
+          Brainwave.io
+        </Link>
+        <nav>
+          <ul className="flex items-center gap-x-10">
+            {navLinks.map((link) => (
+              <li
+                key={link.title}
+                className="font-bold text-sm hover:underline"
+              >
+                <Link href={link.href}>{link.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <Button className="font-bold">Buy now - Starting at $99</Button>
+      </header>
+      <header className="w-screen flex items-center justify-end p-4 relative md:hidden">
+        <input type="checkbox" id="menu-toggle" className="hidden" />
+        <label htmlFor="menu-toggle" className="cursor-pointer block">
+          <button>☰</button>
+        </label>
+      </header>
+    </>
   );
 };
 
